d0755235ec08b0664cf3b9fbebb9dc3fdf51067c
ibalejandro <asanch41@eafit.edu.co>
2017-02-23 20:00:11 -0500

    Uploading the basic skeleton of the lab.

---
+++ Agente_AlejandroS_AlejandroC.py
@@ -0,0 +1,48 @@
+import sys
+
+
+def main():
+    print("-main-")
+    state = [[0, 1, 2], [2, 0, 0], [1, 0, 0]]
+    player = 1
+    action = agent(state, player)
+    print("Put a piece in the position: ")
+    print(action)
+#   print(str(sys.argv))
+
+
+def agent(state, player):
+    print("-agent-")
+    print("State: ")
+    print(state)
+    print("Player: ")
+    print(player)
+    print("State in [1, 0]: ")
+    print(state[1][0])
+
+    player_1_pieces = []
+    player_2_pieces = []
+    empty_fields = []
+    for i in range(0, len(state)):
+        for j in range(0, len(state[i])):
+            position = [i, j]
+            piece_owner = state[i][j]
+            if piece_owner == 1:
+                player_1_pieces.append(position)
+            elif piece_owner == 2:
+                player_2_pieces.append(position)
+            else:
+                empty_fields.append(position)
+
+    print("Player 1 pieces are located in: ")
+    print(player_1_pieces)
+    print("Player 2 pieces are located in: ")
+    print(player_2_pieces)
+    print("Empty fields are located in: ")
+    print(empty_fields)
+
+    return [0, 0]
+
+
+if __name__ == '__main__':
+    main()
