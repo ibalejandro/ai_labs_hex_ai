9e4f21cf2f6e8910787fcf5bfe1f177153387df5
Alejocram <alejocram@gmail.com>
2017-02-27 20:18:59 -0500

    function to increment depth

--- MiniMax.py
+++ MiniMax.py
@@ -3,23 +3,37 @@ import copy
 import label
 import time
 
-MAX_DEPTH = 2
+INCR_VALUE = 96
+INCR_VALUE_2 = 110
+INCR_VALUE_3 = 114
 
-init_state = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+init_state = \
+[[2, 1, 2, 1, 1, 2, 2, 1, 2, 1, 0],
+[1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 0],
+[1, 1, 2, 1, 1, 2, 1, 2, 1, 2, 0],
+[1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 0],
+[1, 1, 2, 1, 1, 2, 1, 2, 1, 2, 0],
+[1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 0],
+[1, 1, 2, 1, 1, 2, 1, 2, 1, 2, 0],
+[1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1],
+[1, 1, 2, 1, 1, 2, 1, 2, 1, 2, 2],
+[1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 2],
+[1, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1]]
 
+max_depth = 2
+is_max_depth_odd = 0
 current_player = -1
+count_turn = 114
+
+def update_max_depth():
+    global max_depth, is_max_depth_odd
+    max_depth += 3
+    is_max_depth_odd = max_depth % 2
 
 def run_minimax(state, player):
+    global count_turn, current_player
+    if count_turn == INCR_VALUE_3:
+        update_max_depth()
     current_player = player
     state_utility = -math.inf
     action = (math.inf, -math.inf)
@@ -33,15 +47,16 @@ def run_minimax(state, player):
                 if min_or_max_value > state_utility:
                     state_utility = min_or_max_value
                     action = (i, j)
+    count_turn += 2
     return action
 
 
 def minimax(row, column, state, depth):
     is_adversary_turn = depth % 2
     state_utilty = (1 if is_adversary_turn else -1) * math.inf
-    if depth == MAX_DEPTH: return calc_eval_function(row, column, state)
-    print("Current state for this MiniMax:")
-    print(state)
+    if depth == max_depth: return calc_eval_function(row, column, state)
+    #print("Current state for this MiniMax:")
+    #print(state)
     for i in range(0, len(state)):
         for j in range(0, len(state[i])):
             piece_owner = state[i][j]
@@ -57,20 +72,26 @@ def minimax(row, column, state, depth):
 
 
 def calc_eval_function(i, j, state):
-    eval_function_result = calc_border_distance(i, j, state) + (6 - count_adjacent_blank_fields(i, j, state))
-    print("State utility reached: " + str(eval_function_result))
-    print("State for the evaluation:")
-    print(state)
+    eval_function_result = calc_border_distance(i, j, state) + get_appropriate_adj_blank_fields(i, j, state)
+    #print("State utility reached: " + str(eval_function_result))
+    #print("State for the evaluation:")
+    #print(state)
     return eval_function_result
 
 
 def calc_border_distance(i, j, state):
     if current_player == label.P1:
-        max_distances = max(j, label.BOARD_SIZE - j - 1)
+        if is_max_depth_odd:
+            max_or_min_distance = min(j, label.BOARD_SIZE - j - 1)
+        else:
+            max_or_min_distance = max(j, label.BOARD_SIZE - j - 1)
     else:
-        max_distances = max(i, label.BOARD_SIZE - i - 1)
+        if is_max_depth_odd:
+            max_or_min_distance = min(i, label.BOARD_SIZE - i - 1)
+        else:
+            max_or_min_distance = max(i, label.BOARD_SIZE - i - 1)
 
-    return max_distances
+    return max_or_min_distance
 
 def count_adjacent_blank_fields(i, j, state):
     adjacent_blank_fields = 0
@@ -89,6 +110,11 @@ def count_adjacent_blank_fields(i, j, state):
 
     return adjacent_blank_fields
 
+def get_appropriate_adj_blank_fields(i, j, state):
+    adjacent_blank_fields = count_adjacent_blank_fields(i, j, state)
+    if not is_max_depth_odd:
+        adjacent_blank_fields = 6 - adjacent_blank_fields
+    return adjacent_blank_fields
 
 
 start = time.time()
